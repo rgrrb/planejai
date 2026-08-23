93672968921b04f246d5c51e6a36a011394dc894
rogeriosrib <cjo.rogerribeiro@gmail.com>
2026-08-23 13:02:17 -0300

    Repositório base da DIO

---
+++ src/router.tsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import { RootLayout } from './components/layout/RootLayout'
+import { SimulationFormPage } from './pages/SimulationFormPage'
+import { SimulationResultsPage } from './pages/SimulationResultsPage'
+
+export const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <SimulationFormPage />,
+      },
+      {
+        path: '/resultado/:id',
+        element: <SimulationResultsPage />,
+      },
+      {
+        path: '/historico',
+        element: <h1>Histórico de Simulações</h1>,
+      },
+    ],
+  },
+])
